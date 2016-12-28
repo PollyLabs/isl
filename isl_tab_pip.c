743ef2b1e79b6f91ba1c782272099785d167a0b8
Oleksandr Zinenko <ftynse@gmail.com>
2016-12-28 10:49:35 +0100

    isl_tab_pip: broaden search for better solutions
    Try to make the first non-zero dimension in the solution less than
    its current constant value rather than make it zero.

--- isl_tab_pip.c
+++ isl_tab_pip.c
@@ -5161,11 +5161,15 @@ static int force_better_solution(struct isl_tab *tab,
 		return n_op;
 	}
 
+	fprintf(stderr, "[isl_tab] forcing better solution on %d\n", i);
+
 	ctx = isl_vec_get_ctx(sol);
 	v = isl_vec_alloc(ctx, 1 + tab->n_var);
 	if (!v)
 		return -1;
 
+// [# original, set all dims until i inclusive to zero]
+#if 0
 	n = i + 1;
 	for (; i >= n_zero; --i) {
 		v = isl_vec_clr(v);
@@ -5173,6 +5177,26 @@ static int force_better_solution(struct isl_tab *tab,
 		if (add_lexmin_eq(tab, v->el) < 0)
 			goto error;
 	}
+#endif
+//  set all dims until i to zero, set i-th dim inferior to its current value
+	v = isl_vec_clr(v);
+	isl_int_set_si(v->el[1 + i], -1);
+	isl_int_sub_ui(v->el[0], sol->el[1 + i], 1);
+	if (add_lexmin_ineq(tab, v->el) < 0)
+	{
+		fprintf(stderr,"[isl_tab] could not add inequality\n");
+		goto error;
+	}
+
+	n = i + 1;
+	--i;
+	for (; i >= n_zero; --i) {
+		v = isl_vec_clr(v);
+		isl_int_set_si(v->el[1 + i], -1);
+		if (add_lexmin_eq(tab, v->el) < 0)
+			goto error;
+	}
+
 
 	isl_vec_free(v);
 	return n;
