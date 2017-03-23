bd2e2a637466964e4574e68336049fe06496e29f
Oleksandr Zinenko <ftynse@gmail.com>
2017-03-23 10:47:05 +0100

    Export isl_schedule_constraints_get_counted_accesses

--- include/isl/schedule.h
+++ include/isl/schedule.h
@@ -121,6 +121,9 @@ __isl_export
 __isl_give isl_union_map *
 isl_schedule_constraints_get_conditional_validity_condition(
 	__isl_keep isl_schedule_constraints *sc);
+__isl_give isl_union_map *
+isl_schedule_constraints_get_counted_accesses(
+	__isl_keep isl_schedule_constraints *sc);
 
 __isl_give isl_schedule_constraints *isl_schedule_constraints_apply(
 	__isl_take isl_schedule_constraints *sc,
