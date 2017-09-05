9c5f5dd347c3a38f4272accf96a57ee6401b166d
Oleksandr Zinenko <git@ozinenko.com>
2017-09-05 13:35:58 +0200

    isl_id: utility functions for name-only identifiers
    Introduce utility functions for named identifiers: "isl_id_from_name"
    constructs an identifier given a name and "isl_id_has_name" checks whether an
    identifier has a name, i.e. whether its "name" field is not NULL. These
    functions allow for manipulating identifiers without user pointers. In
    particular, they will be used to hide user pointers from C++ and Python
    bindings. The "isl_id_has_name" function is necessary because NULL names cannot
    be expressed as strings in bindings.
    
    Signed-off-by: Oleksandr Zinenko <git@ozinenko.com>

--- include/isl/id.h
+++ include/isl/id.h
@@ -20,11 +20,14 @@ uint32_t isl_id_get_hash(__isl_keep isl_id *id);
 
 __isl_give isl_id *isl_id_alloc(isl_ctx *ctx,
 	__isl_keep const char *name, void *user);
+__isl_give isl_id *isl_id_from_name(isl_ctx *ctx,
+	__isl_keep const char *name);
 __isl_give isl_id *isl_id_copy(isl_id *id);
 __isl_null isl_id *isl_id_free(__isl_take isl_id *id);
 
 void *isl_id_get_user(__isl_keep isl_id *id);
 __isl_keep const char *isl_id_get_name(__isl_keep isl_id *id);
+isl_bool isl_id_has_name(__isl_keep isl_id *id);
 
 __isl_give isl_id *isl_id_set_free_user(__isl_take isl_id *id,
 	void (*free_user)(void *user));
